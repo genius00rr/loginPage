7c5a2c9f0e4262dca36dfb141fcb18ff32dfc7e1
genius00rr <97690326+genius00rr@users.noreply.github.com>
2022-10-10 22:08:41 +0530

    10-10-2022
    Login page using HTML, CSS, and JAVASCRIPT for a college project.

---
+++ script.js
@@ -0,0 +1,13 @@
+const x=document.getElementById('login');
+const y=document.getElementById('register');
+const z=document.getElementById('btn');
+function register(){
+    x.style.left="-100%"
+    y.style.left="23%"
+    z.style.left="50%"
+}
+function login(){
+    x.style.left="23%"
+    y.style.left="100%"
+    z.style.left="0";
+}
